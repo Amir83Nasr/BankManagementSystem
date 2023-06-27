3155b4f6156a9e57f908de7fbda2289444d2307b
Amir Hossein Nasrollahi <ah12.nasrollahi@gmail.com>
2023-06-28 02:06:22 +0330

    Amir : 111

--- Src/Function_Account.cpp
+++ Src/Function_Account.cpp
@@ -133,17 +133,8 @@ void display_all_account(const char *name)
     Account account;
 
     ifstream inFile("../Data/Account.dat", ios::binary);
-<<<<<<< HEAD
-    ifstream inFile2("../Data/Customer.dat", ios::binary);
-<<<<<<< HEAD
     
     if (!inFile)
-=======
-    if (!inFile || !inFile2)
->>>>>>> 99c36c94ddcfd51ca5d57a70bc167cd47f3c3e49
-=======
-    if (!inFile)
->>>>>>> d790e02bb850ea03e8aa623000db14fac9273d21
     {
         cout << "File could not be opened! Press any key...";
         return;
